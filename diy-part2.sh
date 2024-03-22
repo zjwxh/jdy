f10cc507531fd95d4573dffaa63a2644e81f7841
zjwxh <61620117+zjwxh@users.noreply.github.com>
2024-03-22 22:56:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,14 +17,14 @@ sed -i 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC
 sed -i "s/R[0-9]\+\.[0-9]\+\.[0-9]\+/&("$(date +%Y-%m-%d)")/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/NeoBird/g' ./package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/NeoBird/g' ./package/base-files/files/bin/config_generate
 
 # 去除默认bootstrap主题
-sed -i 's/[b|B]ootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
+# sed -i 's/[b|B]ootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
 
 # echo "修改wifi名称"
 # sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
